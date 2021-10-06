0a0e03006335ae04095631c8599c5d0ce480aea8
harshal <harshal@harshals-MacBook-Pro.local>
2021-10-06 18:25:36 +0530

    updated server.ts file

--- src/server.ts
+++ src/server.ts
@@ -3,37 +3,71 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { MONGODB_URI, PORT_NUMBER, ENVIRONMENT, ORIGIN } from "./utils/secrets";
 import { createRolesInDB } from "./prepwork";
-const app = express();
-var corsOptions = {
-  origin: ORIGIN,
-};
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.set("port", PORT_NUMBER);
-app.set("env", ENVIRONMENT);
+class Server {
+  public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+    this.mongo();
+  }
 
-if (MONGODB_URI) {
-  mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-      console.log("Successfully connected to MongoDB.");
+  public config(): void {
+    this.app.set("port", PORT_NUMBER);
+    this.app.set("env", ENVIRONMENT);
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cors({ origin: ORIGIN }));
+  }
+
+  public routes(): void {}
+
+  private mongo() {
+    const MONGODB_URI_WITH_CHECK = MONGODB_URI ?? "<fallback_url>";
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("Mongo connection established");
       createRolesInDB();
-    })
-    .catch((err: any) => {
-      console.error("Connection error", err);
-      process.exit();
     });
-}
-app.get("/", (req, res) => {
-  res.send("Well done!");
-});
+    connection.on("reconnected", () => {
+      console.log("Mongo connection re-established");
+    });
+    connection.on("disconnected", () => {
+      console.log("Mongo connection disconnected");
+      console.log("Trying to reconnect with Mongo...");
+      setTimeout(() => {
+        mongoose.connect(MONGODB_URI_WITH_CHECK, {
+          keepAlive: true,
+          socketTimeoutMS: 3000,
+          connectTimeoutMS: 3000,
+        });
+      }, 3000);
+    });
+    connection.on("close", () => {
+      console.log("Mongo connection closed");
+    });
+    connection.on("error", (error: Error) => {
+      console.log("Mongo connection ERROR: " + error);
+    });
+    const run = async () => {
+      await mongoose.connect(MONGODB_URI_WITH_CHECK, {
+        keepAlive: true,
+      });
+    };
+    run().catch((error) => console.error(error));
+  }
 
-app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
+  public start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        this.app.get("port"),
+        this.app.get("env")
+      );
+      console.log("  Press CTRL-C to stop\n");
+    });
+  }
+}
+const server = new Server();
+server.start();
